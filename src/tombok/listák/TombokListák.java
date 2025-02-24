7e59eacd39523cb6e4565f856cdd1b86c337dfb9
szevasz <163414309+TPatya@users.noreply.github.com>
2025-02-24 13:07:58 +0100

    Tombok 1D

---
+++ src/tombok/listák/TombokListák.java
@@ -0,0 +1,36 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package tombok.listák;
+
+/**
+ *
+ * @author patri
+ */
+public class TombokListák {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        int a = 2;
+        int b = 3;
+        
+        //Tömb 1D
+        //Fix méretüek
+        
+        int[] szamok = new int[2];
+        
+        szamok[0] = a;
+        szamok[1] = b;
+        
+        int asd = szamok.length;
+        
+        System.out.println(asd);
+        
+        System.out.println(szamok.length);
+    }   
+    
+}
